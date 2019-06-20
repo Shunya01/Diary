4d786301d947b0714083cae9bc77f8ede9e7cc91
渡邉舜也 <shun.1630.effort@gmail.com>
2019-06-20 16:14:01 +0900

    新規投稿機能をつけました。
    バリデーション機能もつけました。

--- app/Http/Controllers/DiaryController.php
+++ app/Http/Controllers/DiaryController.php
@@ -4,23 +4,49 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Diary;
+use App\Http\Requests\CreateDiary; // 追加
 
 class DiaryController extends Controller
 {
     public function index()
     {
 
+//::でメソッドを呼んでいるのはLaravelのファサードという機能の一つ
+     //order by,get->クエリビルダ
+     //eloquentもクエリビルダもDBを操作する機能
+    $diaries = Diary::orderBy('id', 'desc')->get();
 
-        $diaries = Diary::all();
+
+        // $diaries = Diary::all();
         //allはテーブルの中身を全て取得する
+
 
         // dd($diaries);
         //dd->die and dump
         //var_dumpとexitをまとめて実行してくれる関数
 
         //viewsディレクトリの中のdiariesディレクトリの中のindex(.blade).phpを返す
-        //第二引数に連想配列の形で
+        //第二引数に連想配列の形でviewで使用したいデータをかく
+        //key=viewで使用する変数名、valueが実際の変数
         return view('diaries.index',['diaries' => $diaries]);
 
+    }
+
+    public function create(){
+        // views/diaries/create.blade.phpを表示する
+        return view('diaries.create');
+    }
+
+
+
+    public function store(CreateDiary $request)
+    {
+        $diary = new Diary(); //Diaryモデルをインスタンス化
+
+        $diary->title = $request->title; //画面で入力されたタイトルを代入
+        $diary->body = $request->body; //画面で入力された本文を代入
+        $diary->save(); //DBに保存
+
+        return redirect()->route('diary.index'); //一覧ページにリダイレクト
     }
 }
